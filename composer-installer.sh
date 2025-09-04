176b363c961aa430f33eb947e48e83708c55da96
Dip Chakraborty <dipcb07@gmail.com>
2025-09-04 19:46:52 +0600

    some modify

--- composer-installer.sh
+++ composer-installer.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
+# Composer Installer Script
+# Securely downloads and installs Composer globally on Linux
+# Usage: ./install-composer.sh [version]
+# Example: ./install-composer.sh 2.7.2
 
+set -e
+
+VERSION=${1:-latest}
+
+echo ">> Fetching expected checksum..."
 EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+
+echo ">> Downloading installer..."
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+echo ">> Verifying installer..."
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
     >&2 echo 'ERROR: Invalid installer checksum'
     rm composer-setup.php
     exit 1
 fi
 
-php composer-setup.php --quiet
-RESULT=$?
+echo ">> Installing Composer ($VERSION)..."
+php composer-setup.php --quiet --version=$VERSION
 rm composer-setup.php
-sudo mv composer.phar  /usr/bin/composer
-sudo chmod +x /usr/bin/composer
 
-exit $RESULT
+echo ">> Moving Composer to /usr/local/bin..."
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+
+echo ">> Done. Run 'composer --version' to check installation."
